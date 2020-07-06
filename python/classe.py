c34ae79f2755a20ce0edc773769a68584138a33e
Gustavo Daniel <gusdanieloop@hotmail.com>
2020-07-06 14:22:40 -0300

    agora ta até o final

--- python/classe.py
+++ python/classe.py
@@ -59,8 +59,20 @@ class Mapa:
         for linha in self.mapa:
             print(*linha)
             print()
-            
-            
+
+    def tirar_corpo(self, x, y):
+        self.mapa[x][y] = 0
+        
+    def colocar_corpo(self, x, y):
+        self.mapa[x][y] = 1
+
+    def agentes_carregando(self):
+        agentes = []
+        for agente in self.agentes:
+            if agente.getCarregando():
+                agentes.append(agente)
+        return agentes
+
 class Agente:
     def __init__(self, x, y, visao, mapa):
         self.x = x
@@ -68,6 +80,8 @@ class Agente:
         self.carregando = False
         self.visao = visao
         self.mapa = mapa
+        self.proporcao = 0
+        self.qtd_corpos = 0
 
     def getX(self):
         return self.x
@@ -93,6 +107,19 @@ class Agente:
     def setCarregando(self, carregando):
         self.carregando = carregando
 
+    def getProporcao(self):
+        return self.proporcao
+    
+    def setProporcao(self, proporcao):
+        self.proporcao = proporcao
+
+    def getQtd_corpos(self):
+        return self.qtd_corpos
+
+    def setQtd_corpos(self, qtd_corpos):
+        self.qtd_corpos = qtd_corpos
+
+
     def mover(self):
         while(True):
             x_ = random.randint(-1, 1)
@@ -103,20 +130,85 @@ class Agente:
                 self.x = x
                 self.y = y
                 break
-
-    def maratona(self, x, y):
-        if(self.x - x < 0 or self.y - y < 0 or self.x + x >= self.mapa.getLinhas() or self.y + y >=self.mapa.getColunas())
-            return False
-        return True
     
-    def verificar(self):
+    def interagir(self):
+        self.verificar_corpos_ao_redor()
+        #print(self.getProporcao())
+        if self.getProporcao() >= 0.4:
+            # print("MUITO CORPO")
+            if self.carregando:
+                # print("CARREGANDO")
+                if not self.mapa.mapa[self.x][self.y]:
+                    # print("deixou o corpo")
+                    # print(f"x = {self.x}, y = {self.y}")
+                    self.mapa.colocar_corpo(self.x, self.y)
+                    self.carregando = False
+                else:
+                    # print("TEM FORMIGA AQUI, mover")
+                    self.mover()
+            else:
+                # print("NAO CARREGANDO, MOVER")
+                self.mover()
+        else:
+            self.getProporcao()
+            # print("POUCO CORPO")
+            if self.carregando:
+                # print("CARREGANDO, MOVER")
+                self.mover()
+            else:
+                if self.mapa.mapa[self.x][self.y]:
+                    # print("tirou o corpo")
+                    # print(f"x = {self.x}, y = {self.y}")
+                    self.mapa.tirar_corpo(self.x, self.y)
+                    self.carregando = True
+                else:
+                    # print("moveu")
+                    self.mover()
+    
+    def interagir_final(self):
+        self.verificar_corpos_ao_redor()
+        if self.getProporcao() >= 0.1:
+            # print("MUITO CORPO")
+            if self.carregando:
+                # print("CARREGANDO")
+                if not self.mapa.mapa[self.x][self.y]:
+                    # print("deixou o corpo")
+                    # print(f"x = {self.x}, y = {self.y}")
+                    self.mapa.colocar_corpo(self.x, self.y)
+                    self.carregando = False
+                else:
+                    # print("TEM FORMIGA AQUI, mover")
+                    self.mover()
+            else:
+                # print("NAO CARREGANDO, MOVER")
+                self.mover()
+        else:
+            self.mover()
+    
+    def verificar_corpos_ao_redor(self):
         qtd_corpos = 0
-        sinal = []
-        for i in range(self.visao):
-            for j in range(self.visao):
-                if self.maratona(i, j):
+        qtd_blocos = 0
+        for i in range(-self.visao, self.visao + 1):
+            for j in range(-self.visao, self.visao + 1):
+                x = self.x + i
+                y = self.y + j
+                if((x >= 0 and x < self.mapa.linhas) and y >= 0 and y < self.mapa.colunas):
+                    qtd_corpos += self.mapa.mapa[x][y]
+                    qtd_blocos += 1
+        self.setProporcao((qtd_corpos-1)/(qtd_blocos))
+        self.setQtd_corpos(qtd_corpos)
 
-        if self.x == 0:
-            if self.y == 0:
-                for i in range(self.visao):
-                    for j in range()
+    def verificar2_corpos_ao_redor(self):
+        qtd_corpos = 0
+        qtd_blocos = 0
+        min_x = max(0, self.x - self.visao)
+        min_y = max(0, self.y - self.visao)
+        max_x = min(self.x + self.visao, self.mapa.linhas - 1)
+        max_y = min(self.y + self.visao, self.mapa.colunas - 1)
+        for i in range(min_x, max_x+1):
+            for j in range(min_y, max_y+1):
+                qtd_corpos += self.mapa.mapa[i][j]
+                qtd_blocos += 1
+        self.setProporcao(qtd_corpos/qtd_blocos)
+        self.setQtd_corpos(qtd_corpos)
+                        
--- python/main.py
+++ python/main.py
@@ -11,16 +11,35 @@ qtd_corpos = int(tam_mapa*corpos)
 qtd_agentes = int(tam_mapa*agentes)
 
 mapa = Mapa(linha, coluna, qtd_corpos, qtd_agentes, tam_mapa)
-print(mapa.getAgentes(0).getX(), mapa.getAgentes(0).getY())
+'''print(mapa.getAgentes(0).getX(), mapa.getAgentes(0).getY())
 mapa.getAgentes(0).mover()
 print(mapa.getAgentes(0).getX(), mapa.getAgentes(0).getY())
+'''
+mapa.show_mapa()
 
-#mapa.show_mapa()
+print("#####################################################")
 
-# for i in range():
-#     while(True):
-#         x = random.randint(0, linha-1)
-#         y = random.randint(0, coluna-1)
-#         if mapa[x][y] == 0:
-#             mapa[x][y] = 2
-#             break
+
+for i in range(25000):
+    #print("i =",i)
+    for j in range(qtd_agentes):
+        #print("j =", j)
+        mapa.getAgentes(j).interagir()
+mapa.show_mapa()
+
+print("#####################################################")
+
+carregando = mapa.agentes_carregando()
+
+#nao_carregando = []
+while(carregando):
+    for i, agente in enumerate(carregando):
+        agente.interagir_final()
+        if not agente.getCarregando():
+            print(f"Agente {i} não está mais carregando")
+            carregando.pop(i)
+        else:
+            print(f"Agente {i} ainda carregando")
+print("\n\n\n")
+mapa.show_mapa()
+        
